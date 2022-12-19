5c294b71294882d9214bfe46bcec69fd8168ae10
lijing2 <lijing2@tetras.ai>
2022-12-19 15:15:41 +0800

    更新promise面试题

---
+++ ！补充题：promise经典面试题/await等的不是promise.js
@@ -0,0 +1,44 @@
+async function async1() {
+  console.log('async1 start');
+  await async2(); 
+  console.log('async1 end');
+}
+
+async function async2() {
+  console.log('async2');
+}
+
+console.log('script start');
+
+setTimeout(function () {
+  console.log('setTimeout');
+}, 0);
+
+async1();
+
+new Promise(function (resolve) {
+  console.log('promise1');
+  resolve();
+  console.log('promise2');
+}).then(function () {
+  console.log('promise3');
+});
+
+console.log('script end');
+
+// 注意 第三行await等的 不是promise，先把await后面的代码（async1 end）放到微任务，再执行async外面的同步代码，再把 promise3 放到微任务队列
+
+// 宏任务
+// script start
+// async1 start
+// async2
+// promise1
+// promise2
+// script end
+
+// 微任务
+// async1 end
+// promise3
+
+// 下一轮 
+// setTimeout
