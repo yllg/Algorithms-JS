d41fe91811e98a9bef234081b42197a6ec5cd1a7
lijing <lijingkf@codemao.cn>
2023-02-12 21:11:56 +0800

    chore: 补充注释

--- babel相关/手写插件/数字预计算插件/test.js
+++ babel相关/手写插件/数字预计算插件/test.js
@@ -5,3 +5,5 @@ const result = babel.transform("const result = 1 + 2 + 3 +4 +5;", {
 });
 
 console.log(result.code); // const result = 15;
+
+// 注意：小数计算会有误差的问题，可用1KB都不到的number-precision库，或5.9KB的big.js库来修正；
